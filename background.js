9fcd524a8db6c15c4b1ef0fc273efafbd9510940
sjegink <sjegink@gmail.com>
2024-08-30 11:39:26 +0900

    feat: An Injection example

--- background.js
+++ background.js
@@ -12,6 +12,17 @@ chrome.tabs.onActivated.addListener((activeInfo) => {
 
 function onTabActive() {
 	chrome.tabs.query({ active: true, currentWindow: true }, async function ([tab]) {
+		/* 
+		// Here is an example!
+		if (/^https:\/\/([^.]*\.)?chatgpt.com\//.test(tab.url)) {
+			await injectContentJS(tab);
+			return Promise.all([
+				injectResource(tab.id, 'style', `html/fake-gpt-ui.css`),
+				injectResource(tab.id, 'html', `html/fake-gpt-ui.html`),
+				// This is not working: injectResource(tab.id, 'js', `html/fake-gpt-ui.js`),
+			]);
+		}
+		*/
 		console.log('NOTHING TO DO on this page', tab.url);
 	});
 }
@@ -20,5 +31,11 @@ function injectContentJS(tab) {
 	return chrome.scripting.executeScript({
 		target: { tabId: tab.id },
 		files: ['content.js'],
+	});
+}
+
+function injectResource(tabId, resourceType, resourcePath){
+	return fetch(chrome.runtime.getURL(resourcePath)).then(resp => resp.text()).then(data => {
+		return chrome.tabs.sendMessage(tabId, { action: resourceType, data });
 	});
 }
--- content.js
+++ content.js
@@ -4,22 +4,44 @@ if (!window._$_CONTENT_SCRIPT_INJECTED_$_) {
 	chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
 		switch (message.action) {
 			case 'hello':
-				console.log("ChromeExtension says Hello World!");
+				console.log('ChromeExtension says:', message.data ?? 'Hello World!');
 				break;
 			case 'style':
 				{
 					const CUSTOM_STYLEELEMENT_CLASSNAME = "chrome-extension-style-injected";
 					document.head.querySelectorAll(`style.${CUSTOM_STYLEELEMENT_CLASSNAME}`).forEach(styleElement => styleElement.parentNode.removeChild(styleElement));
-					let styleElement = document.createElement('style');
+					const styleElement = document.createElement('style');
 					styleElement.classList.add(CUSTOM_STYLEELEMENT_CLASSNAME);
 					styleElement.innerHTML = message.data;
-					document.head.appendChild(styleElement)
+					document.head.appendChild(styleElement);
+					console.log('A style element has been injected.');
 				}
 				break;
+			case 'html': {
+				{
+					const CUSTOM_ELEMENT_CLASSNAME = "chrome-extension-html-injected";
+					document.body.querySelectorAll(`.${CUSTOM_ELEMENT_CLASSNAME}`).forEach(styleElement => styleElement.parentNode.removeChild(styleElement));
+					const injectingElement = document.createElement('div');
+					injectingElement.classList.add(CUSTOM_ELEMENT_CLASSNAME);
+					injectingElement.innerHTML = message.data;
+					document.body.appendChild(injectingElement);
+					console.log('A html element has been injected.');
+				}
+				break;
+			}
+			case 'js':
+				{
+					eval(String(function () {
+						const err = new Error(`Refused to evaluate a string as JavaScript because 'unsafe-eval' is not an allowed source of script in the following Content Security Policy directive: "script-src 'self' 'wasm-unsafe-eval' 'inline-speculation-rules' http://localhost:* http://127.0.0.1:*".`);
+						err.name = 'EvalError';
+						throw err;
+					}));
+				}
+				return sendResponse(false);
 			default:
 				console.log('unknown message listen', message);
 		}
-		sendResponse('ㅇㅅㅇ');
+		sendResponse(true);
 	});
 }
 
