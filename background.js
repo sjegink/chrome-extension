b8601ff57e1e9ed534f9ae08a20d82dc773b29b9
sjegink <sjegink@gmail.com>
2024-10-30 17:58:21 +0900

    feat: darktheme for calendar,docswave

--- background.js
+++ background.js
@@ -21,6 +21,26 @@ function onTabActive() {
 				data: cssData,
 			});
 		}
+		if (/^https:\/\/calendar\.google\.com\/calendar\/u\//.test(tab.url)) {
+			await injectContentJS(tab);
+			const cssData = await fetch(chrome.runtime.getURL(`css/calendar.google.com.css`)).then(resp => resp.text());
+			console.log({ cssData });
+			return chrome.tabs.sendMessage(tab.id, {
+				action: 'style',
+				data: cssData,
+			});
+		}
+		if ([
+			/https:\/\/www\.docswave\.com\//,
+		].map(re => re.test(tab.url)).includes(true)) {
+			await injectContentJS(tab);
+			const cssData = await fetch(chrome.runtime.getURL(`css/dark-invert.css`)).then(resp => resp.text());
+			console.log({ cssData });
+			return chrome.tabs.sendMessage(tab.id, {
+				action: 'style',
+				data: cssData,
+			});
+		}
 		console.log('NOTHING TO DO on this page', tab.url);
 	});
 }
